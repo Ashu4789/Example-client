6e960d42f471ec4ee6b9893db44d23735638da6a
ashu <ashutoshmohanty2004@gmail.com>
2026-02-11 11:18:54 +0530

    feat: add Manage Users page and navigation
    - Created [ManageUsers.jsx](cci:7://file:///C:/Users/ashut/OneDrive/Desktop/MERNtarining/example-client/src/pages/ManageUsers.jsx:0:0-0:0) to list users with edit/delete actions.
    - Added `/manage-users` route to [App.jsx](cci:7://file:///C:/Users/ashut/OneDrive/Desktop/MERNtarining/example-client/src/App.jsx:0:0-0:0).
    - Added "Manage Users" link to the user dropdown in `UserHeader.jsx`.

--- src/App.jsx
+++ src/App.jsx
@@ -12,109 +12,123 @@ import { useSelector, useDispatch } from "react-redux";
 import { SET_USER } from "./redux/user/action";
 import Groups from "./pages/Groups";
 import GroupExpenses from "./pages/GroupExpenses";
+import ManageUsers from "./pages/ManageUsers";
 
 function App() {
-    const dispatch = useDispatch();
-    // Value of userDetails represents whether the user
-    // is logged in or not.
+  const dispatch = useDispatch();
+  // Value of userDetails represents whether the user
+  // is logged in or not.
 
-    // useSelector takes in 1 function as input. Redux calls the function that
-    // you pass to useSelector with all the values its storing/managing.
-    // We need to take out userDetails since we're interested in userDetails object.
-    const userDetails = useSelector((state) => state.userDetails);
-    const [loading, setLoading] = useState(true);
+  // useSelector takes in 1 function as input. Redux calls the function that
+  // you pass to useSelector with all the values its storing/managing.
+  // We need to take out userDetails since we're interested in userDetails object.
+  const userDetails = useSelector((state) => state.userDetails);
+  const [loading, setLoading] = useState(true);
 
-    const isUserLoggedIn = async () => {
-        try {
-            const response = await axios.post(
-                `${serverEndpoint}/auth/is-user-logged-in`,
-                {},
-                { withCredentials: true }
-            );
+  const isUserLoggedIn = async () => {
+    try {
+      const response = await axios.post(
+        `${serverEndpoint}/auth/is-user-logged-in`,
+        {},
+        { withCredentials: true }
+      );
 
-            // setUserDetails(response.data.user);
-            dispatch({
-                type: SET_USER,
-                payload: response.data.user,
-            });
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        isUserLoggedIn();
-    }, []);
-
-    if (loading) {
-        return (
-            <div className="container text-center">
-                <h3>Loading...</h3>
-            </div>
-        );
+      // setUserDetails(response.data.user);
+      dispatch({
+        type: SET_USER,
+        payload: response.data.user,
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
+  useEffect(() => {
+    isUserLoggedIn();
+  }, []);
+
+  if (loading) {
     return (
-        <Routes>
-            <Route
-                path="/"
-                element={
-                    userDetails ? (
-                        <Navigate to="/dashboard" />
-                    ) : (
-                        <AppLayout>
-                            <Home />
-                        </AppLayout>
-                    )
-                }
-            />
-            <Route
-                path="/login"
-                element={
-                    userDetails ? (
-                        <Navigate to="/dashboard" />
-                    ) : (
-                        <AppLayout>
-                            <Login />
-                        </AppLayout>
-                    )
-                }
-            />
-
-            <Route
-                path="/dashboard"
-                element={
-                    userDetails ? (
-                        <UserLayout>
-                            <Groups />
-                        </UserLayout>
-                    ) : (
-                        <Navigate to="/login" />
-                    )
-                }
-            />
-
-            <Route
-                path="/groups/:groupId"
-                element={
-                    userDetails ? (
-                        <UserLayout>
-                            <GroupExpenses />
-                        </UserLayout>
-                    ) : (
-                        <Navigate to="/login" />
-                    )
-                }
-            />
-
-            <Route
-                path="/logout"
-                element={userDetails ? <Logout /> : <Navigate to="/login" />}
-            />
-        </Routes>
+      <div className="container text-center">
+        <h3>Loading...</h3>
+      </div>
     );
+  }
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          userDetails ? (
+            <Navigate to="/dashboard" />
+          ) : (
+            <AppLayout>
+              <Home />
+            </AppLayout>
+          )
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          userDetails ? (
+            <Navigate to="/dashboard" />
+          ) : (
+            <AppLayout>
+              <Login />
+            </AppLayout>
+          )
+        }
+      />
+
+      <Route
+        path="/dashboard"
+        element={
+          userDetails ? (
+            <UserLayout>
+              <Groups />
+            </UserLayout>
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+
+      <Route
+        path="/groups/:groupId"
+        element={
+          userDetails ? (
+            <UserLayout>
+              <GroupExpenses />
+            </UserLayout>
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+
+      <Route
+        path="/manage-users"
+        element={
+          userDetails ? (
+            <UserLayout>
+              <ManageUsers />
+            </UserLayout>
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+
+      <Route
+        path="/logout"
+        element={userDetails ? <Logout /> : <Navigate to="/login" />}
+      />
+    </Routes>
+  );
 }
 
 export default App;
